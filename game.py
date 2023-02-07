91d445f0018c8eae780492ec2e6956eb9774cf11
Esther <esterdataengineer@gmail.com>
2023-02-07 20:18:19 +0200

    Doing a commit to work on the project from another pc.

--- game.py
+++ game.py
@@ -12,44 +12,7 @@ class Game:
 
     def run(self):
         while self.is_running:
-            self.p1.play_turn()
-            self.p2.play_turn()
             self.board.print_board()
-            # row_index = Player.validate_input(
-            #                                   list_to_validate= self.board.state,
-            #                                   message = "please enter your chosen row from 0-7.\n",
-            #                                   name_of_list = "board",
-            #                                   piece_color=  cur_player)
-            # row = self.board.state[row_index]
-            # piece_index = Player.validate_input(
-            #                                     list_to_validate = row,
-            #                                     message = "please choose a piece on this row.\n",
-            #                                     name_of_list = "row",
-            #                                     piece_color = cur_player)
-            # # while True:
-            #     row = input()
-            #     col = input()
-            #     cur_cell = self.board.state[row][col]
-            #     if isinstance(cell, Piece) and cell.color == player.color:
-            #         break
-            #     print()
-            # # while True:
-            #     row = input()
-            #     col = input()
-            #     next_cell = self.board.state[row][col]
-            #     if isempty
-            #         break
-            #     print()
-            cur_cell = ""
-            # next_cell = Piece(cur_player.color)
-
-            # if cur_player == self.p1:
-            #     next_row = self.board.state[row_index + 1]
-            # else:
-            #     next_row = self.board.state[row_index - 1]
-            # next_row_index = Player.validate_input(next_row, "please choose where you want to move your piece, from 0-7.\n",
-            #                                 "next_row", cur_player)
-            # next_row[next_row_index] = Piece(cur_player.color)
-            # row[piece_index] = " "
-            # cur_player = Game.change_player(cur_player, self.p1, self.p2)
-            # break
+            self.p1.play_turn(self.board)
+            self.p2.play_turn(self.board)
+            self.board.print_board()
--- game_objects/player.py
+++ game_objects/player.py
@@ -1,3 +1,6 @@
+from game_objects.pieces import Piece
+
+
 class Player:
     def __init__(self, color):
         self.color = color
@@ -5,8 +8,23 @@ class Player:
     def __str__(self):
         return f"{self.color}"
 
-    def play_turn(self):
-        # def __choose_piece
-        # __choose_piece
-        # __choose_piece
-        return
+    @staticmethod
+    def is_valid(input_value):
+        if input_value.isdigit() and (1 <= int(input_value) <= 8):
+            return True
+        return False
+
+    def input_cell(self):
+        while True:
+            row = input("please enter a row (1-8):\n")
+            col = input("please enter a cell (1-8):\n")
+            if self.is_valid(row) and self.is_valid(col):
+                break
+        return int(row) - 1, int(col) - 1
+
+    def play_turn(self, board):
+        while True:
+            piece_row, piece_col = self.input_cell()
+            cell = board.state[piece_row][piece_col]
+            if isinstance(cell, Piece) and cell.color == self.color:
+                break
